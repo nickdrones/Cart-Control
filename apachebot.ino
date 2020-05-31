35bdabc45be6a5a2967c1838dffde5096f2c5ea7
Nick B <nickbelbas@earthlink.net>
2020-05-31 11:08:07 -0500

    added 7-segment display to display power percentage

--- apachebot.ino
+++ apachebot.ino
@@ -1,4 +1,5 @@
 #include <TM1637Display.h> //import library for 7-segment diaplsy
+#include <Arduino.h>
 
 //Initializing pins on Arduino Pro Mini
 int forwardPin = 4;  //forward side of dead-man rocker switch
@@ -9,7 +10,7 @@ float mappedVal;  //temp calculated variable for potentiometer value
 
 float linear; //float with calculated linear value of log pot
 
-const int numReadings = 30;
+const int numReadings = 10;
 
 int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
@@ -22,22 +23,33 @@ float middleSpace = 1500; //middle motor pulse (no movement)
 float incrementSpeed = 5; //how fast to increment motor speed by
 float decrementSpeed = 20; //how fast to decrement motor speed by
 
+#define CLK 2  //connector pins for I2C for 7-seg display
+#define DIO 3
+
 float max = 0; //min PWM pulse val
 float min = 2000; //max PWM pulse val
+
+TM1637Display display(CLK, DIO); //init 7-seg display object to pins
+
+uint8_t data[] = { 0xff, 0xff, 0xff, 0xff }; //hex vals for all segments on, holds current val to be shown
 
 void setup() {
   pinMode(forwardPin, INPUT);
   pinMode(backwardPin, INPUT); //Init the pins as in or out
   pinMode(PWM_out, OUTPUT);
-
   pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.begin(9600);
   for (int thisReading = 0; thisReading < numReadings; thisReading++) { //initialize whole buffer for log pot readings to 0 (part of moving average filter)
     readings[thisReading] = 0;
   }
+  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 }; //hex vals for all segments off
+  display.setSegments(blank); //Set display to blank
 }
 void loop() {
+  uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 }; //hex vals for all segments off
+  display.setBrightness(0x0f); //set brightness to max
+
   while (not digitalRead(forwardPin) && not digitalRead(backwardPin)) { //while move button is not on
     smoothDialVal();
     digitalWrite(LED_BUILTIN, LOW);
@@ -49,7 +61,8 @@ void loop() {
       motorPulse += decrementSpeed;
     }
     sendMSpulse(motorPulse);             //send pulse to motor
-    Serial.println(motorPulse);
+    //Serial.println(motorPulse);
+    writeToDisplay();
   }
 
   while (digitalRead(forwardPin)) //while forward is pressed on switch
@@ -59,8 +72,8 @@ void loop() {
     }
     sendMSpulse(motorPulse);    //send pulse to motor
     digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println(motorPulse);
-
+    //Serial.println(motorPulse);
+    writeToDisplay();
   }
 
   while (digitalRead(backwardPin))  //while backward is pressed on switch
@@ -70,10 +83,9 @@ void loop() {
     }
     sendMSpulse(motorPulse);      //send pulse to motor
     digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println(motorPulse);
-
+    //Serial.println(motorPulse);
+    writeToDisplay();
   }
-
 }
 bool rockerForward()
 {
@@ -103,6 +115,18 @@ void smoothDialVal() {
   }
   average = total / numReadings;
 }
+
+void writeToDisplay() {
+  int percentage = map((log(analogRead(A0))*1000), 5140, 6930, 0, 100);
+  //data[0] = display.encodeDigit(0);
+  //data[1] = display.encodeDigit(int(percent / 100));
+  //data[2] = display.encodeDigit(int(percent / 10));
+  //data[3] = display.encodeDigit(int(percent % 10));
+  display.showNumberDec(percentage, false);
+  Serial.println(log(analogRead(A0))*1000);
+  //display.setSegments(data);
+}
+
 void sendMSpulse(int pulseLength) { //send PWM pulse to motor
   digitalWrite(PWM_out, HIGH);
   delayMicroseconds(pulseLength);
